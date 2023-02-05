20b79af2ecfdbd9848e2ead20efffef3c36c72a0
Chad Kluck Dev <kl5@chadkluck.net>
2023-02-05 14:12:10 -0600

    chore: added umwug data

--- app/dao-games.js
+++ app/dao-games.js
@@ -1,24 +1,24 @@
 
 const games = {
-	"gamechoices": [
+	gamechoices: [
 		"Falken's Maze",
-		"Black Jack",
-		"Gin Rummy",
-		"Hearts",
-		"Bridge",
-		"Checkers",
-		"Chess",
-		"Poker",
-		"Fighter Combat",
-		"Guerrilla Engagement",
-		"Desert Warfare",
-		"Air-To-Ground Actions",
-		"Theaterwide Tactical Warfare",
-		"Theaterwide Biotoxic and Chemical Warfare",
-		"Global Thermonuclear War"
+		'Black Jack',
+		'Gin Rummy',
+		'Hearts',
+		'Bridge',
+		'Checkers',
+		'Chess',
+		'Poker',
+		'Fighter Combat',
+		'Guerrilla Engagement',
+		'Desert Warfare',
+		'Air-To-Ground Actions',
+		'Theaterwide Tactical Warfare',
+		'Theaterwide Biotoxic and Chemical Warfare',
+		'Global Thermonuclear War'
 	],
-	"hiddengames": [
-		"Tic-Tac-Toe"
+	hiddengames: [
+		'Tic-Tac-Toe'
 	]
 };
 
---
+++ app/dao-umwug.js
@@ -0,0 +1,73 @@
+
+const dataSet = {
+    default: [ 'Hello, UMWUG!'],
+    bao: ['banana', 'apples', 'oranges'],
+    cbl: [
+        {
+            name: 'Charlie Brown',
+            id: '1005783',
+            fines: 38.93
+        },
+        {
+            name: 'Linus van Pelt',
+            id: '1004378',
+            fines: 0,
+            loans: [ {title: 'Bible (KJV)', due: '20181226'}, { title: 'Public Speaking', due: '20181226'} ]
+        }
+    ],
+    lvp: {
+        name: 'Linus van Pelt',
+        id: '1004378',
+        fines: 0,
+        loans: [ {title: 'Bible (KJV)', due: '20181226'}, { title: 'Public Speaking', due: '20181226'} ]
+    },
+    cpe: {
+        gamechoices: [
+            "Falken's Maze",
+            'Black Jack',
+            'Gin Rummy',
+            'Hearts',
+            'Bridge',
+            'Checkers',
+            'Chess',
+            'Poker',
+            'Fighter Combat',
+            'Guerrilla Engagement',
+            'Desert Warfare',
+            'Air-To-Ground Actions',
+            'Theaterwide Tactical Warfare',
+            'Theaterwide Biotoxic and Chemical Warfare',
+            'Global Thermonuclear War'
+        ],
+        hiddengames: [
+            'Tic-Tac-Toe'
+        ]
+    },
+    dev: [ 'https://developers.exlibrisgroup.com' ],
+    doc: [ 'https://developers.exlibrisgroup.com/alma/apis/users' ],
+    lhf: [
+        'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/{{user_id}}/loans?user_id_type=all_unique&limit=10&offset=0&order_by=due_date&format=json&direction=ASC&apikey={{apikey}}',
+	    'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/{{user_id}}/requests?request_type=HOLD&user_id_type=all_unique&limit=10&offset=0&status=active&format=json&apikey={{apikey}}',
+	    'https://api-na.hosted.exlibrisgroup.com/almaws/v1/users/{{user_id}}/fees?user_id_type=all_unique&status=ACTIVE&format=json&apikey={{apikey}}'
+    ],
+    git: [
+        'https://github.com/ustlibraries',
+        'https://github.com/chadkluck',
+        'https://github.com/chadkluck/serverless-sam-8ball-example',
+        'https://github.com/chadkluck/php-project-framework',
+        'https://github.com/chadkluck/js-template'
+    ],
+    bns: [
+        'https://api-na.hosted.exlibrisgroup.com/primo/v1/pnxs?q={{query}}&lang=eng&offset=1&limit=10&view=full&vid=STTHOMAS&scope=stthomas&apikey={{apikey}}',
+	    'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/?view=full&expand=p_avail,e_avail,d_avail &nz_mms_id={{nz_mms_id}}&format={{format}}&apikey={{apikey}}',
+	    'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/{{mms_id}}/holdings?format={{format}}&apikey={{apikey}}',
+	    'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/{{mms_id}}/holdings/{{holding_id}}/items/?format={{format}}&apikey={{apikey}}',
+	    'https://api-na.hosted.exlibrisgroup.com/almaws/v1/bibs/{{mms_id}}/holdings/{{holding_id}}/items/{{item_pid}}/requests?user_id={{user_id}}&format={{format}}&apikey={{apikey}}'
+    ]
+}
+
+const getData = function (code) {
+    let key = (code !== null && code !== '') ? code.toLowerCase() : 'default';
+    key = ( (key in dataSet) ? key : 'default' );
+	return dataSet[key];
+};
